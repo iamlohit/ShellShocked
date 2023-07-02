c97e4c30f1dcf7cf94f8a1088ea2a054127e05f1
Lohit <jethwanilohit@gmail.com>
2023-07-02 12:07:13 +0530

    Just changed -l to -F since script asked for files only.

--- 13_functions_with_arguments.sh
+++ 13_functions_with_arguments.sh
@@ -3,7 +3,7 @@
 function file_count() {
     local dir=$1
     echo "${dir}:"
-    local no_of_lines=$(ls -l ${dir} | wc -l) && echo $no_of_lines
+    local no_of_lines=$(ls -F ${dir} | wc -l) && echo $no_of_lines
 }
 
 file_count $1
